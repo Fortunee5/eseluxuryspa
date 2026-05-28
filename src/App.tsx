c6dc22b3732520fe4e3c729d89a641f292c01f02
Fortune <amoschegwe@gmail.com>
2026-05-28 22:29:24 +0100

    cs erre solve

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
 // Layouts & Components
 // @ts-ignore - Temporary bypass if your components are still .jsx
